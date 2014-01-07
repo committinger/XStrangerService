2a3fd207fa3dbae468e872a95d37e5c229398cc4
Narcissus Du <narcissusdu@gmail.com>
2014-01-07 09:33:19 +0800

    first commit of the project

---
+++ 30_SourceCode/XStrangerService/Interfaces/ServiceInterface/ServiceContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Committinger.XStrangerService.ServiceInterface
+{
+   public class ServiceContext
+    {
+        public const string SERVICECONTRACT_NAMESPACE = "http://www.committinger.com/xstrangerservice/servicecontract";
+        public const string DATACONTRACT_NAMESPACE = "http://www.committinger.com/xstrangerservice/datacontract";
+    }
+}
